78114c9a2ef15c867add199747935040f02dbe5b
Justin Bartels <justinbartels@unomaha.edu>
2025-04-11 17:27:28 -0500

    firebase setup, basic push to database

--- app/pages/TestPage.tsx
+++ app/pages/TestPage.tsx
@@ -1,10 +1,34 @@
-import { TestButton } from "~/components/TestButton";
+import { useRef } from "react";
+import { firestore } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 export function TestPage() {
+    const messageRef = useRef<HTMLInputElement>(null);
+    const ref = collection(firestore,"test");
+
+    const handleSave = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        if(messageRef.current) {
+            console.log(messageRef.current.value)
+            let data = {
+                message:messageRef.current.value,
+            }
+            try {
+                addDoc(ref,data)
+            } catch (e) {
+               console.log(e) 
+            }
+        }
+ 
+    }
+
     return (
         <div>
-         <p>Wow look at this  </p>
-         <TestButton />
+         <form onSubmit={handleSave}>
+            <label>Enter message</label>
+            <input type="text" ref={messageRef} defaultValue=""/>
+            <button type="submit">Save</button>
+         </form>
         </div>
     );
 }
