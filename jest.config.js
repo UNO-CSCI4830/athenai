8854ed8336cd1ae52e8bde95285d54dff697a602
BlakeBritton1 <blakebritton0906@outlook.com>
2025-05-02 20:40:28 -0500

    Jest fixes

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: [`${__dirname}/jest.setup.cjs`],
+};
